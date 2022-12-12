ac2f06351a44e47239b8ebe84180285609985f96
Wojciech Dzikowski <dzikowskw@gmail.com>
2022-12-12 19:15:45 +0100

    AoC Day 12 Part 2 refactored

--- aoc2022/12.py
+++ aoc2022/12.py
@@ -1,63 +1,45 @@
 import numpy as np
 
+E = chr(ord('z')+1)
+S = chr(ord('a')-1)
+WALL_CHAR = chr(127)
+neighbors = [[-1, 0], [1, 0], [0, -1], [0,1]]
 
-def bfs(input, sx, sy, end = 'E'):
-    x = sx
-    y = sy
-    smallest = -1
+def bfs(input1, start, end, cmpr=lambda c,lastc: ord(c) > ord(lastc) + 1):
+    input = np.pad(input1, 1, constant_values=WALL_CHAR)
+    
+    x, y = [n[0] for n in np.where(input == start)]
     visited = np.zeros_like(input, dtype=int)
-    xlen, ylen = input.shape
-    next = [[sx,sy, 0, None]]
+    visited[:,[0,-1]] = visited[[0,-1]] = 1
+    next = [[x,y, 0, None]]
+    
     while len(next) > 0:
         x,y, d, lastc = next.pop(0)
-        if x < 0 or x >= xlen or y < 0 or y >= ylen:
-            continue
         if visited[x][y] > 0:
             continue
         c = input[x,y]
-        if c == 'S': c = chr(ord('a')-1)
-        if lastc is not None and input[x,y] != end and ord(c) > ord(lastc) + 1:
-            continue
-        if lastc != 'z' and input[x,y] == end:
-            continue
-        visited[x][y] = d+1
-        if input[x][y] == end:
-            return d
-        next.append([x-1, y, d+1, c])
-        next.append([x+1, y, d+1, c])
-        next.append([x, y-1, d+1, c])
-        next.append([x, y+1, d+1, c])
+        if lastc is None or cmpr(c, lastc):
+            visited[x][y] = d+1
+            if input[x][y] == end:
+                return d
+            for n in neighbors:
+                next.append([x + n[0], y + n[1], d+1, c])
     
-    return 0
-
-
-def calc(input):
-    #find start
-    sx, sy = [n[0] for n in np.where(input == 'S')]
-
-    #bfs
-    return bfs(input, sx, sy, 'E')
-
-def calcPart2(input):
-    sx, sy = [n[0] for n in np.where(input == 'S')]
-    input[sx,sy] = 'a'
-    
-    res = []
-    xlen, ylen = input.shape
-    for xx in range(0,xlen):
-        for yy in range(0, ylen):
-            if (input[xx, yy] == 'a'):
-                path = bfs(input, xx, yy, 'E')
-                if (path > 0):
-                    res.append(path)
-        
-    #bfs
-    return min(res)
-
+    assert(False)
+   
 
 with open("aoc2022/12.txt") as f:
     input = np.array([[*c] for c in [x.strip() for x in f]])
-
-    print('part 1', calc(input))
-    print('part 2', calcPart2(input))
+    
+    sx, sy = [n[0] for n in np.where(input == 'S')]
+    input[sx,sy] = S
+    ex, ey = [n[0] for n in np.where(input == 'E')]
+    input[ex,ey] = E
+    #part 1
+    print('Part 1:', bfs(input, S, E, lambda c,prevc: ord(c) <= ord(prevc) + 1))
+    
+    #part 2
+    sx, sy = [n[0] for n in np.where(input == S)]
+    input[sx,sy] = 'a'
+    print('Part 2:', bfs(input, E, 'a', lambda c, prevc: ord(prevc) <= ord(c) + 1))
 
