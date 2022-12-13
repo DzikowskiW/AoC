e2ff5e741d698135cf9f6beca465a8c11c0de69f
Wojciech Dzikowski <dzikowskw@gmail.com>
2022-12-13 07:06:13 +0100

    AoC Day 13 Part 1 working

--- aoc2022/13.py
+++ aoc2022/13.py
@@ -3,7 +3,12 @@
 def compare(l, r):
     #print('compare', l, ' vs ', r)
     if isinstance(l, int) and isinstance(r, int):
-        assert(False)
+        if (l == r):
+            return 0
+        elif (l < r):
+            return 1
+        else:
+            return -1
     elif isinstance(l, list) and isinstance(r,list):
         i = 0
         minlen = min(len(l), len(r))
@@ -11,19 +16,15 @@ def compare(l, r):
             ll = l[i]
             rr = r[i]
             i+=1
-            if isinstance(ll, int) and isinstance(rr, int):
-                #print('compare', ll, ' vs ', rr)
-                if (ll == rr):
-                    continue
-                elif (ll < rr):
-                    return True
-                else:
-                    return False
-            if not compare(ll,rr):
-                return False
-        if len(l) == i:
-            return True
-        return False
+            cmpres = compare(ll,rr)
+            if cmpres != 0:
+                return cmpres
+        if len(l) == len(r):
+            return 0
+        elif len(l) == minlen:
+            return 1
+        else:
+            return -1
     elif isinstance(l, list) and isinstance(r, int):
         return compare(l, [r])
     elif isinstance(l, int) and isinstance(r, list):
@@ -36,7 +37,7 @@ def firstCompare(l,r):
     print(l, '\n', r, '\n' )
     res = compare(l, r)
     print(res)
-    return res
+    return res == 1
 
 with open("aoc2022/13.txt") as f:
     input = [x.strip() for x in f]
