b01f6efe898b182301625ff24c7412bcdb3bce0e
Wojciech Dzikowski <dzikowskw@gmail.com>
2022-12-16 08:02:58 +0100

    AoC 2022 Day 15 Part 2

--- aoc2022/15.py
+++ aoc2022/15.py
@@ -26,48 +26,90 @@ class Sensor:
             return True
         return False
     
+    def checkBorderPoints(self, lmin, lmax, sensors):
+        dist = self.dist +1
+        for i in range(0, dist+1):
+            p = []
+            if lmin <= (self.sx - dist + i) <= lmax and lmin <= self.sy - i <= lmax:
+                p.append([self.sx - dist + i,self.sy - i])
+            if lmin <= (self.sx - dist + i) <= lmax and lmin <= self.sy + i <= lmax:
+                p.append([self.sx - dist + i,self.sy + i])
+            if lmin <= (self.sx + dist + i) <= lmax and lmin <= self.sy + i <= lmax:
+                p.append([self.sx + dist - i,self.sy + i])
+            if lmin <= (self.sx + dist + i) <= lmax and lmin <= self.sy - i <= lmax:
+                p.append([self.sx + dist - i,self.sy - i])
+            for pp in p:
+                isIn = False
+                for s in sensors:
+                    if not isIn and s!= self and s.withinRange(pp[0],pp[1]):
+                        isIn = True
+                if not isIn:
+                    print('RESULT', pp)
+                    print('RESULT', pp[0]*4000000 + pp[1])
+                    return True        
+        return False
+    
     def __repr__(self) -> str:
         return '{('+str(self.sx)+','+str(self.sy)+'):'+ str(self.dist)+'}'
 
 def calcDist(x1, y1, x2, y2):
     return abs(x1 - x2) + abs(y1 - y2)
 
-def plotSensor(cave, s, offsetX, offsetY):
-    #calculate distanse to beacon
-    dist = calcDist(*s)
-    for xx in range(0, len(cave)):
-        for yy in range(0, len(cave[0])):
-            if (calcDist(s[0]+ offsetX, s[1] + offsetY, xx , yy)) <= dist:
-                cave[xx, yy] = '1'
-    return     
+def analyzePoint(x,y, sensors):
+    c = '.'
+    for s in sensors:
+        # if c in ['S', 'B']:
+        #     break
+        if s.onSensor(x, y):
+            if (c in ['.', '#']): 
+                c = 'S'
+                break
+            else: assert(False)
+        elif s.onBeacon(x, y):
+            if (c in ['.', '#']): 
+                c = 'B'
+                break
+            else: assert(False)
+        elif s.withinRange(x, y):
+            if (c == '.'): 
+                c = '#'
+    return c
+
+def analyzeSensors(sensors, bmin = 0, bmax = 20):
+    pointsToCheck = []
+    for s in sensors:
+        if (s.checkBorderPoints(bmin, bmax, sensors)): 
+            return
+        print('sensor ', s)
+    #print(checked)
+
+
+def analyzeSensors1(sensors, bmin = 0, bmax = 20):
+    pointsToCheck = []
+    for s in sensors:
+        dist = s.dist+1
+        pointsToCheck += [[s.sx - dist, s.sy], [s.sx+dist, s.sy], [s.sx, s.sy-dist], [s.sx, s.sy+dist]]
+    for p in pointsToCheck:
+        if bmin <= p[0] <= bmax and bmin <= p[1] <= bmax:
+            if analyzePoint(p[0],p[1],sensors) == '.':
+                print(p)
+                return
 
 def analyzeRow(row, sensors, minX, maxX):
     sum = 0
     strow = ''
+    cx = None
     for xx in range(minX, maxX):
-        c = '.'
-        for s in sensors:
-            # if c in ['S', 'B']:
-            #     break
-            if s.onSensor(xx, row):
-                if (c in ['.', '#']): 
-                    c = 'S'
-                    break
-                else: assert(False)
-            elif s.onBeacon(xx, row):
-                if (c in ['.', '#']): 
-                    c = 'B'
-                    break
-                else: assert(False)
-            elif s.withinRange(xx, row):
-                if (c == '.'): 
-                    c = '#'
+        c = analyzePoint(xx, row, sensors)
         if c == '#':
             sum += 1
+        elif c == '.':
+            cx = xx
         strow += c
-    
-    print(sum)
-    #print(strow)
+        
+    #print(sum)
+    print(strow)
+    return cx  
 
 with open("aoc2022/15.txt") as f:
     lines = [x.strip() for x in f]
@@ -92,12 +134,21 @@ with open("aoc2022/15.txt") as f:
         if maxX < int(xs): maxX = int(xs)
         i+=1
     
-    print(minX, maxX)
-    maxDist = max([a.dist for a in sensors])
-    print(maxDist)
-    # for y in range(-10,30):
-    #     analyzeRow(y, sensors, -10, 35)
-    analyzeRow(2000000, sensors, -1031499-maxDist-5, 3994355+maxDist+5)
+    # print(minX, maxX)
+    # maxDist = max([a.dist for a in sensors])
+    # print(maxDist)
+    
+    #dim = 20
+    dim = 4000000
+    
+    # point = None
+    # for y in range(0, dim+1):
+    #     xx = analyzeRow(y, sensors, 0, dim+1)
+    #     if xx is not None:
+    #         point = [xx, y]
+    # print(point[0], point[1])
+    analyzeSensors(sensors, 0, dim)
+    #analyzeRow(2000000, sensors, -1031499-maxDist-5, 3994355+maxDist+5)
     
     
     
