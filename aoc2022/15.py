2b6ba150baff4145f5dea72389f284bef344af56
Wojciech Dzikowski <dzikowskw@gmail.com>
2022-12-16 09:23:19 +0100

    AoC 2022 Day 15 - refactored, far from optimal runtime

--- aoc2022/15.py
+++ aoc2022/15.py
@@ -58,8 +58,6 @@ def calcDist(x1, y1, x2, y2):
 def analyzePoint(x,y, sensors):
     c = '.'
     for s in sensors:
-        # if c in ['S', 'B']:
-        #     break
         if s.onSensor(x, y):
             if (c in ['.', '#']): 
                 c = 'S'
@@ -81,74 +79,52 @@ def analyzeSensors(sensors, bmin = 0, bmax = 20):
         if (s.checkBorderPoints(bmin, bmax, sensors)): 
             return
         print('sensor ', s)
-    #print(checked)
-
-
-def analyzeSensors1(sensors, bmin = 0, bmax = 20):
-    pointsToCheck = []
-    for s in sensors:
-        dist = s.dist+1
-        pointsToCheck += [[s.sx - dist, s.sy], [s.sx+dist, s.sy], [s.sx, s.sy-dist], [s.sx, s.sy+dist]]
-    for p in pointsToCheck:
-        if bmin <= p[0] <= bmax and bmin <= p[1] <= bmax:
-            if analyzePoint(p[0],p[1],sensors) == '.':
-                print(p)
-                return
 
 def analyzeRow(row, sensors, minX, maxX):
     sum = 0
     strow = ''
-    cx = None
     for xx in range(minX, maxX):
-        c = analyzePoint(xx, row, sensors)
+        c = '.'
+        for s in sensors:
+            if s.onSensor(xx, row):
+                if (c in ['.', '#']): 
+                    c = 'S'
+                    break
+                else: assert(False)
+            elif s.onBeacon(xx, row):
+                if (c in ['.', '#']): 
+                    c = 'B'
+                    break
+                else: assert(False)
+            elif s.withinRange(xx, row):
+                if (c == '.'): 
+                    c = '#'
         if c == '#':
             sum += 1
-        elif c == '.':
-            cx = xx
         strow += c
-        
-    #print(sum)
-    print(strow)
-    return cx  
+    
+    print('PART 1: ', sum)
+    # print(strow)
 
 with open("aoc2022/15.txt") as f:
     lines = [x.strip() for x in f]
     input = np.zeros([len(lines), 4], dtype=int)
     sensors = []
-    i=0
-    maxY = -1000
-    minY = 1000
-    
-    maxX = -1000
-    minX = 1000
     for line in lines:
         xs, ys, xb, yb = re.search(r"Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)", line).groups()
-        sensors.append(Sensor([xs, ys, xb, yb]))
-        if minY > int(ys): minY = int(ys)
-        if minY > int(yb): minY = int(yb)
-        if maxY < int(yb): maxY = int(yb)
-        if maxY < int(ys): maxY = int(ys)
-        if minX > int(xb): minX = int(xb)
-        if minX > int(xs): minX = int(xs)
-        if maxX < int(xb): maxX = int(xb)
-        if maxX < int(xs): maxX = int(xs)
-        i+=1
+        sensors.append(Sensor([xs, ys, xb, yb]))    
+
+    #part 1
+    print('--- PART 1 ---')
+    maxDist = max([a.dist for a in sensors])
+    analyzeRow(2000000, sensors, -1031499-maxDist-5, 3994355+maxDist+5)
+    #analyzeRow(10, sensors, -5, 27)
     
-    # print(minX, maxX)
-    # maxDist = max([a.dist for a in sensors])
-    # print(maxDist)
-    
+    #part 2
+    print('--- PART 2 ---')
     #dim = 20
     dim = 4000000
-    
-    # point = None
-    # for y in range(0, dim+1):
-    #     xx = analyzeRow(y, sensors, 0, dim+1)
-    #     if xx is not None:
-    #         point = [xx, y]
-    # print(point[0], point[1])
     analyzeSensors(sensors, 0, dim)
-    #analyzeRow(2000000, sensors, -1031499-maxDist-5, 3994355+maxDist+5)
     
     
     
