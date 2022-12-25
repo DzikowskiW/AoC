56d582f7f9915bc9ae04235400854e2993705185
Wojciech Dzikowski <dzikowskw@gmail.com>
2022-12-25 11:07:20 +0100

    AoC 2022 Day 17 Part 2

--- aoc2022/17.py
+++ aoc2022/17.py
@@ -1,6 +1,16 @@
 import numpy as np
-
 BLOCKS = [
+    [(0,0), (1,0), (2,0), (3,0)],
+    [(1,0), (0,1), (1,1), (2,1), (1,2)],
+    [(0,0), (1,0), (2,0), (2,1), (2,2)],
+    [(0,0), (0,1), (0,2), (0,3)],
+    [(0,0), (1,0), (0,1), (1,1)]
+
+]
+BLOCKS_H = [1,3,3,4,2]
+BLOCKS_W = [4,3,3,1,2]
+
+BLOCKS1 = [
 np.array([[1,1,1,1]]),
 np.array([
     [0,1,0],
@@ -19,74 +29,117 @@ np.array([
 ])
 ]
 
-def moveHorizontal(x,y,dir,block, stack):
-    by, bx = block.shape
-    sy, sx = stack.shape
+def overlaps(x,y,block,stack):
+    for b in block:
+        if (b[0]+x, b[1]+y) in stack:
+            return True
+    return False
+
+def moveHorizontal(x,y,dir,block,bx, stack):
     if dir == '<':
         if x == 0: 
             return x, y
-        if np.count_nonzero(stack[y:y+by, x-1:x-1+bx] & block) > 0:
+        if overlaps(x-1, y, block, stack):
             return x,y
         return x-1, y
     if dir == '>':
-        if x+bx == sx: 
+        if x+bx == 7: 
             return x, y
-        if np.count_nonzero(stack[y:y+by, x+1:x+1+bx] & block) > 0:
+        if overlaps(x+1, y, block, stack):
             return x,y
         return x+1, y
 
-def moveDown(x,y,block, stack):
-    by, bx = block.shape
-    sy, sx = stack.shape
-    if y+by == sy: 
+def moveDown(x, y, block, by, stack):
+    if y == 1:
         return False
-    if np.count_nonzero(stack[y+1:y+by+1, x:x+bx] & block) > 0:
+    if overlaps(x,y-1,block, stack):
         return False
     return True
 
-def visualise(miny, maxy, stack,block):
-    cut = stack[miny:maxy][:]
-    cy,cx = cut.shape
-    for y in range(0, cy):
+def visualise(miny, maxy, stack):
+    output = ''
+    for y in range(maxy,miny-1,-1):
         line = ''
-        for x in range(0,cx):
-            if cut[y,x] == 1: line += '#'
-            if cut[y,x] == 0: line += '.'
-        print(line)
-    
+        for x in range(0,7):
+            if (x,y) in stack:
+                line += '#'
+            else:
+                line += '.'
+        output += line
+    return output
 
-def part1(input):
-    stack = np.zeros((5000, 7), dtype=int)
-    sy, sx = stack.shape
+def part2(input, iterations):
+    v= len(input)*7*5
+    res = tetris(input, iterations % v)
+    if (iterations > v):
+        v0 = tetris(input, v)
+        print(v0)
+        v1 = tetris(input, v*2)
+        print(v1-v0, v1)
+        v2 = tetris(input, v*3)
+        print(v2-v1, v2)
+        v3 = tetris(input, v*4)
+        print(v3-v2, v3)
+        v4 = tetris(input, v*5)
+        print(v4-v3, v4)
+        vv = v2 - v1
+    res += vv * int(iterations / v)
+    print(res)
+
+def tetris(input, iterations = 2022):
+    cache = {}
+    maxCombinations = iterations % len(input) * 5
+    
+    stack = set()
     i = 0
     curBlock = 0
-    topy = sy
-    for it in range(0, 2022):
-
+    maxy = 0
+    padY = 0
+    it = 0 
+    useCycle = True
+    while it < iterations:
         block = BLOCKS[curBlock]
-        by, bx = block.shape
+        bx = BLOCKS_W[curBlock]
+        by = BLOCKS_H[curBlock]
         #start place
-        x, y = 2, topy - 3 - by
+        x, y = 2, maxy + 4
         
         while True:    
-            x, y = moveHorizontal(x, y, input[i], block, stack)
-            if moveDown(x, y, block, stack):
-                y += 1
+            x, y = moveHorizontal(x, y, input[i], block,bx, stack)
+            if moveDown(x, y, block, by, stack):
+                y -= 1
             else:
                 i = (i+1) % len(input)
                 break
             i = (i+1) % len(input)
 
         # add block 
-        for xx in range(0, bx):
-            for yy in range(0, by):
-                stack[y + yy, x + xx] = max(block[yy,xx], stack[y + yy, x + xx])
-        topy = min(topy, y)
+        for b in block:
+            stack.add((b[0]+x, b[1]+y))
+        maxy= max(maxy, y+by-1)
+        
+        #find cycle
+        cacheKey = (curBlock, i, visualise(maxy-100,maxy,stack))
+        if cacheKey in cache and useCycle:
+            cycle = it - cache[cacheKey][0]
+            cycleVal = maxy - cache[cacheKey][1]
+            padY = ((iterations - it) // cycle)*cycleVal
+            it += ((iterations - it) // cycle)*cycle
+            useCycle = False
+        else:    
+            cache[cacheKey] = (it, maxy)
+        
         #next block
         curBlock = (curBlock + 1) % len(BLOCKS)
-    print('height', sy-topy)
+        it += 1
+    return maxy + padY
 
 
 with open("aoc2022/17.txt") as f:
     input = [x.strip() for x in f][0]
-    part1(input)
+    tmp = set()
+    print('PART 1')
+    print(tetris(input, 2022))
+    print('PART 2')
+    print(tetris(input, 1000000000000))
+    
