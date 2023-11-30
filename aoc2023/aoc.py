53407c49a5f44aa2472d0259530d68e5651fe31a
Wojciech Dzikowski <dzikowskw@gmail.com>
2023-11-30 19:34:21 +0100

    AoC 2023 setup

---
+++ aoc2023/aoc.py
@@ -0,0 +1,17 @@
+# aoc.py
+from typing import List
+
+def input_as_string(filename:str) -> str:
+    """returns the content of the input file as a string"""
+    with open(filename) as f:
+        return f.read().rstrip("\n")
+
+def input_as_lines(filename:str) -> List[str]:
+    """Return a list where each line in the input file is an element of the list"""
+    return input_as_string(filename).split("\n")
+
+def input_as_ints(filename:str) -> List[int]:
+    """Return a list where each line in the input file is an element of the list, converted into an integer"""
+    lines = input_as_lines(filename)
+    line_as_int = lambda l: int(l.rstrip('\n'))
+    return list(map(line_as_int, lines))
