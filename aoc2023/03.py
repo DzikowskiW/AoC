7e10e55c0f27510f3241f20a9bbf530d49714c35
Wojciech Dzikowski <dzikowskw@gmail.com>
2023-12-03 08:23:38 +0100

    AoC 2023 Day 3 working solution

--- aoc2023/03.py
+++ aoc2023/03.py
@@ -2,60 +2,47 @@ import aoc
 import re
 import numpy as np
 
-def getNumber(engine, x,y, stopy=-1):
-    numm = str(engine[x][y])
-    yl = y-1
-    while engine[x][yl].isdigit():
-        numm = str(engine[x][yl]) + numm
-        yl -= 1
-    yr = y+1
-    while engine[x][yr].isdigit():
-        numm += engine[x][yr]
-        yr += 1
-    return (int(numm), yl,yr)
+#return full number with its start coords 
+def get_full_number(engine, row, col):
+    numm = str(engine[row][col])
+    cl = col - 1
+    while engine[row][cl].isdigit():
+        numm = str(engine[row][cl]) + numm
+        cl -= 1
+    cr = col + 1
+    while engine[row][cr].isdigit():
+        numm += engine[row][cr]
+        cr += 1
+    return (int(numm), row, cl)
 
-def findPartNumbers(engine, x, y):
-    nums = set()
-    for xx in range(x-1, x+2):
-        if xx < 0 or xx >= engine.shape[0]:
-                break
-        for yy in range(y-1, y+2):
-            if (engine[xx][yy].isdigit()):
-                numm = getNumber(engine, xx, yy)
-                nums.add(numm)
-    sum = 0
-    for n in nums:
-        sum+=n[0]
-    return sum      
-
-#first part
-def loop1(lines):
-    print('part 1')
-    lines1 = list(map(lambda l: [ *l], lines))
+# returns numbers surrounding (row,col) point
+def find_part_numbers(engine, row, col):
+    unique_numbers = set()
+    for r in range(row - 1, row + 2):
+        for c in range(col - 1, col + 2):
+            if engine[r][c].isdigit():
+                full_number = get_full_number(engine, r, c)
+                unique_numbers.add(full_number)
+    return list(map(lambda n: n[0], unique_numbers))
     
-    engine = np.pad(np.array(lines1),1, constant_values='.')
-    summ = 0
-    nums = set()
+def loop(lines):
+    lines = list(map(lambda l: [ *l], lines))
+    engine = np.pad(lines,1, constant_values='.')
+    part1 = 0
+    part2 = 0
     for x in range(engine.shape[0]):
         for y in range(engine.shape[1]):
+            #part 1
             if engine[x][y] != '.' and not engine[x][y].isdigit():
-                summ += findPartNumbers(engine, x, y)
-                # print('num', engine[x][y], x, y, numm)
-                # nums = nums.union(numm)
-    print(nums)
-    for nn in nums:
-        summ += nn[0]
-    print(summ)
-    
-    
-    
-    
-    
-#second part
-def loop2(lines):    
-    print('part 2')
-    # print('part 2:', summ)
-    
+                part1 += sum(find_part_numbers(engine, x, y))
+            #part 2
+            if engine[x][y] == '*':
+                nums = find_part_numbers(engine, x, y)
+                if len(nums) == 2:
+                    part2 += nums[0] * nums[1]
+    print('part 1:', part1)
+    print('part 2:', part2)
+
 lines = aoc.input_as_lines("input/03.txt")
-loop1(lines)
+loop(lines)
 
