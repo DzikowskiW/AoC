f137d391d07729e6a080a450f487e8c12b2b8443
Wojciech Dzikowski <dzikowskw@gmail.com>
2023-12-04 17:15:43 +0100

    AoC 2023 Day 4 Working solutions

--- README.md
+++ README.md
@@ -47,3 +47,14 @@ for count, value in enumerate(values):
 a = [1, 2, 3, 4]
 print(mathprod(a)) # 24
 ```
+
+`@functools.cache` - memoization of function results, [documetation link](https://docs.python.org/3/library/functools.html)
+```
+import functools
+
+@functools.cache
+def factorial(n):
+    return n * factorial(n-1) if n else 1
+```
+
+intertools - useful library for efficient looping [documentation link](https://docs.python.org/3/library/itertools.html)
--- aoc2023/04.py
+++ aoc2023/04.py
@@ -1,24 +1,50 @@
 import aoc
-import re
+import functools
+from collections import defaultdict
 
-def loop(lines):
-    summ = 0   
-    for line in lines:
+def process_next_cards(lines, number):
+    score = 0
+    winners, numbers = lines[number].split(': ')[1].split(' | ')
+    winners = [ int(w) for w in winners.split(' ') if len(w) > 0 ]
+    numbers = [ int(n) for n in numbers.split(' ') if len(n) > 0 ]
+    for n in numbers:
+        if n in winners:
+            score +=1
+    return list(range(number + 1, number + score + 1))
+
+score_cache = dict()
+def count_score(index, cards):
+    if index not in score_cache: 
+        score = 1
+        for c in cards[index]:
+            score += count_score(c, cards)
+        score_cache[index] = score
+    return score_cache[index]
+
+def loop2(lines):
+    counter = 0
+    cards = defaultdict(list)
+    for i in range(len(lines)):
+        cards[i] = process_next_cards(lines, i)
+    for i in range(len(cards)):
+        counter += count_score(i, cards)
+    print('part 2:', counter)
+    
+def loop1(lines):
+    points = defaultdict(int)  
+    for i, line in enumerate(lines):
         winners, numbers = line.split(': ')[1].split(' | ')
-        winners = winners.split(' ')
-        numbers = numbers.split(' ')
-        score = 0
+        winners = [ int(w) for w in winners.split(' ') if len(w) > 0 ]
+        numbers = [ int(n) for n in numbers.split(' ') if len(n) > 0 ]
         for n in numbers:
-            if len(n) > 0 and n in winners:
-                if score == 0: score = 1
-                else: score *= 2
-        summ += score
-        print(numbers)
-        print(winners)
-        print(score)
-    print(summ)
+            if n in winners:
+                if i not in points: 
+                    points[i] = 1
+                else: 
+                    points[i] *= 2
+    print('part 1:', sum(points.values()))
         
-
 lines = aoc.input_as_lines("input/04.txt")
-loop(lines)
+loop1(lines)
+loop2(lines)
 
