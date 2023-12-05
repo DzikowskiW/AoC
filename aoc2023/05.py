d11240300e9a13aacb5caffc3df53b543fa93ea5
Wojciech Dzikowski <dzikowskw@gmail.com>
2023-12-05 18:41:47 +0100

    AoC 2023 Day 5

--- aoc2023/05.py
+++ aoc2023/05.py
@@ -3,7 +3,6 @@ import functools
 import re
 import math
 from collections import defaultdict
-    
 
 def loop1(seeds, lines):
     res = 0
@@ -20,11 +19,7 @@ def loop1(seeds, lines):
         else:
             dest, src, l = list(map(int, line.split(' ')))
             types[curStep].append((src,dest,l))
-            # for j in range(l):
-            #     types[curStep][src +j] = dest + j 
-        print('aa')
     
-    print('minLoc')
     minLocation =  math.inf
     for s in seeds:
         nextVal = s
@@ -35,13 +30,56 @@ def loop1(seeds, lines):
                     nextVal = dest + (nextVal - src)
                     break
             t = nextSteps[t]
-        print(t, nextVal)
         minLocation = min(nextVal, minLocation)
-    
-    # print(nexśtSteps['seed'], types['seed'][0])
     print('part 1:', minLocation)
+    
+def in_seed_range(n, range):
+    for r in range:
+        if r[0]<= n and r[1]> n:
+            return True    
+    return False
+
+def loop2(seeds, lines):
+    # seed ranges
+    seed_ranges = []
+    for i in range(0, len(seeds)-1, 2):
+        seed_ranges.append((seeds[i], seeds[i]+seeds[i+1]))
+    seed_ranges = sorted(seed_ranges)
+    
+    prevSteps = {}
+    curStep = ''
+    nextStep = ''
+    types = defaultdict(list)
+    for line in lines:
+        if len(line) == 0:
+            curStep = ''
+        elif curStep == '':
+            curStep, nextStep = line.split(' ')[0].split('-to-')
+            prevSteps[nextStep] = curStep
+        else:
+            dest, src, l = list(map(int, line.split(' ')))
+            types[curStep].append((dest,src,l))
+    
+    ii = 0
+    while True:
+        t = 'location'
+        res = ii
+        while True:
+            for src, dest, l in types[t]:
+                if res >= src and res < src + l:
+                    res = dest + (res - src)
+                    break
+            if t not in prevSteps:
+                break;
+            t = prevSteps[t]
+        if in_seed_range(res, seed_ranges): 
+            print('part2', ii, res)
+            return
+        ii += 1
+
         
 lines = aoc.input_as_lines("input/05.txt")
 seeds = list(map(int,re.findall(r'\d+', lines[0])))
 lines = lines[2:]
 loop1(seeds, lines)
+loop2(seeds, lines)
