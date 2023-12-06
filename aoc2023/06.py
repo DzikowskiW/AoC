2862be1c9494f556415d23228661cce5fffc22ae
Wojciech Dzikowski <dzikowskw@gmail.com>
2023-12-06 07:33:08 +0100

    AoC 2023 Day 6

--- aoc2023/06.py
+++ aoc2023/06.py
@@ -1,27 +1,39 @@
 import aoc
-import functools
 import re
-import math
-from collections import defaultdict
 
-def loop1(times, distances):
-    print(times, distances)
+def loop(times, distances):
     res = 1
     for i in range(0,len(times)):
-        ways = 0
+        start = -1
+        end = -1
+        
+        #find start
         for j in range(times[i]):
-            for x in range (1, times[i] - j+1):
-               if x*j > distances[i]:
-                   print(j, x, x*j)
-                   ways += 1 
-                   break
-        # print(times[i], distances[i], ways)
-        res *= ways
+            x = times[i] - j
+            if x*j > distances[i]:
+                start = j
+                break;
+        
+        #find end
+        for j in range(times[i]-1, 0, -1):
+            x = times[i] - j
+            if x*j > distances[i]:
+                end = j
+                break;
+        
+        res *= (end - start + 1)
     print(res)
                 
-                
-    
 lines = aoc.input_as_lines("input/06.txt")
+
+#part 1
 times = list(map(int,re.findall(r'\d+', lines[0])))
 distances = list(map(int,re.findall(r'\d+', lines[1])))
-loop1(times, distances)
+print('part 1:')
+loop(times, distances)
+
+#part 2
+times = list(map(int,re.findall(r'\d+', lines[0].replace(" ", ""))))
+distances = list(map(int,re.findall(r'\d+', lines[1].replace(" ", ""))))
+print('part 2:')
+loop(times, distances)
