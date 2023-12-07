ed63da43ba3f47108e1292ec939b97d68f489280
Wojciech Dzikowski <dzikowskw@gmail.com>
2023-12-07 18:19:50 +0100

    AoC 2023 Day 7 both parts

--- aoc2023/07.py
+++ aoc2023/07.py
@@ -1,60 +1,63 @@
 import aoc
-import re
 from collections import defaultdict
 
-cards = '23456789TJQKA'
-cardsV= '23456789abcdefghijk'
+CARDS = '23456789TJQKA'
+CARD_VALS= '23456789abcdefghijk'
+CARD_VALS_W_JOKER= '23456789a1cdefghijk'
 
-cardCache = dict()
-def cardValue(card, cardsN=cards, cardsV=cardsV):
+def card_score(card, is_joker = False, cards=CARDS, card_vals=CARD_VALS):
+    score = ''
     
-    print('----')
-    print(card)
-    res = ''
-    cards = defaultdict(int)
+    #count card occurences
+    card_counter = defaultdict(int)
+    num_of_jokers = 0
     for c in card:
-        cards[c] += 1
+            if c != 'J' or not is_joker:
+                card_counter[c] += 1
+            else:
+                num_of_jokers += 1;
+    card_counts = sorted(list(card_counter.values()), reverse=True)
     
-    cardvals = list(cards.values())
-    if 5 in cards.values():
-        res = '7|'
-    elif 4 in cards.values():
-        res = '6|'
-    elif 3 in cards.values() and 2 in cards.values():
-        res = '5|'
-    elif 3 in cards.values():
-        res = '4|'
-    elif cardvals.count(2) == 2:
-        res = '3|'
-    elif 2 in cardvals:
-        res = '2|'
-    elif 1 in cardvals:
-        res = '1|'
-    res +=  ''.join(cardsV[cardsN.rfind(c[0])] for c in card)
-    print(cardvals)
-    print(res)
-    return res
+    #score card
+    if (len(card_counts) == 0):
+        return '7' + ''.join(card_vals[cards.rfind(c[0])] for c in card)
+    card_counts[0] += num_of_jokers
+    if 5 in card_counts:
+        score = '7'
+    elif 4 in card_counts:
+        score = '6'
+    elif 3 in card_counts and 2 in card_counts:
+        score = '5'
+    elif 3 in card_counts:
+        score = '4'
+    elif card_counts.count(2) == 2:
+        score = '3'
+    elif 2 in card_counts:
+        score = '2'
+    elif 1 in card_counts:
+        score = '1'
+    score +=  ''.join(card_vals[cards.rfind(c[0])] for c in card)
+    return score
     
-        
-
 def compareCards(card1, card2):
-   return cardValue(card1[0]) - cardValue(card2[0])
+   return card_score(card1[0]) - card_score(card2[0])
 
-def loop(lines):
+def loop(lines, jokers = False):
+    #parse line as tuple (hand, bid)
     lines = list(map(lambda l: l.split(" "), lines))
     lines = list(map(lambda l: (l[0], int(l[1])), lines))
+
+    #score and sort cards
+    card_vals= CARD_VALS_W_JOKER if jokers else CARD_VALS
     for i, l in enumerate(lines):
-        lines[i] = (cardValue(l[0]), l[0], l[1])
+        lines[i] = (card_score(l[0], is_joker=jokers, card_vals=card_vals), l[0], l[1])
     hand = sorted(lines)
-    res = 0
-    for i, h in enumerate(hand, start=1):
-        print(h)
-        res += i*h[2]
+    
+    res = sum([i*h[2] for i, h in enumerate(hand, start=1)])
     print(res)
+
 lines = aoc.input_as_lines("input/07.txt")
-
 print('part 1:')
-loop(lines)
-
-# 250754618 too low
-# 250951660
+loop(lines, jokers = False)
+print('part 2:')
+loop(lines, jokers = True)
