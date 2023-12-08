d6d952f79e7efd60c38a17536421d4407e54b573
Wojciech Dzikowski <dzikowskw@gmail.com>
2023-12-08 07:56:54 +0100

    AoC 2023 Day 8 both parts

--- aoc2023/08.py
+++ aoc2023/08.py
@@ -1,28 +1,58 @@
 import aoc
 import re
+import math
 from collections import defaultdict
 
-
-def loop(steps, lines):
+def part2(steps, lines):
     count = 0
-    step = 0
+    current_step = 0
     m = defaultdict()
-    node = 'AAA'
-    print(steps, lines)
-    for i, l in enumerate(lines):
-        a, b = l.split(" = ")
+    nodes = []
+    node_counter = defaultdict(int)
+    
+    #parse input
+    for line in lines:
+        a, b = line.split(" = ")
+        l, r = b[1:-1].split(", ")
+        m[a] = {'L':l,'R':r}
+        if a[2] == 'A':
+            nodes.append(a)
+    
+    #find step counter for each node
+    for ii, node in enumerate(nodes):
+        while node[2] != 'Z':
+            node_counter[ii]  += 1
+            node = m[node][steps[current_step]]
+            current_step = (current_step + 1) % len(steps)
+
+    #lowest common multiplier
+    count = math.lcm(*list(node_counter.values()))
+    print(count)
+
+
+def part1(steps, lines):
+    count = 0
+    current_step = 0
+    m = defaultdict()
+    current_node = 'AAA'
+    for line in lines:
+        a, b = line.split(" = ")
         l, r = b[1:-1].split(", ")
         m[a] = {'L':l,'R':r}
 
-    while node != 'ZZZ':
+    while current_node != 'ZZZ':
         count += 1
-        node = m[node][steps[step]]
-        step = (step+1) % len(steps)
+        current_node = m[current_node][steps[current_step]]
+        current_step = (current_step + 1) % len(steps)
     print(count)
                 
 
 lines = aoc.input_as_lines("input/08.txt")
-lines
+
 #part 1
 print('part 1:')
-loop(lines[0], lines[2:])
+part1(lines[0], lines[2:])
+
+#part 1
+print('part 2:')
+part2(lines[0], lines[2:])
