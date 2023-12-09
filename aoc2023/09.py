cf0bf2ec5c2836823137b0026806f8267ebdd3f0
Wojciech Dzikowski <dzikowskw@gmail.com>
2023-12-09 06:53:02 +0100

    AoC 2023 day 9 both parts

--- aoc2023/09.py
+++ aoc2023/09.py
@@ -4,35 +4,31 @@ from copy import deepcopy
 import math
 from collections import defaultdict
 
+def parseSequence(line):
+    seq = [[* line]]
+    while not all(map(lambda h: h == 0, seq[-1])):
+        seq.append([])
+        for i in range(1, len(seq[-2])):
+            seq[-1].append(seq[-2][i] - seq[-2][i-1])
+    seq[-1].append(0)
+    return seq
 
-def predict(line):
-    histories = [[* line]]
-    print(histories)
-    while not all(map(lambda h: h == 0, histories[-1])):
-        histories.append([])
-        for i in range(1, len(histories[-2])):
-            histories[-1].append(histories[-2][i] - histories[-2][i-1])
-        print(histories[-1])
-    histories[-1].append(0)
-    for i in range(len(histories) -2, -1, -1):
-        histories[i].append(histories[i][-1] + histories[i+1][-1])
-    print(list(map(lambda l: l[-1], histories)))
-    return histories[0][-1]
+def predictPrev(line):
+    seq = parseSequence(line)
+    for i in range(len(seq) -2, -1, -1):
+        seq[i].insert(0,seq[i][0] - seq[i+1][0])
+    return seq[0][0]
 
-def part1(lines):
-    res= 0
-    predict(lines[0])
-    for l in lines:
-        res += predict(l)
-    print(res)
-            
-        
-                
+def predictNext(line):
+    seq = parseSequence(line)
+    for i in range(len(seq) -2, -1, -1):
+        seq[i].append(seq[i][-1] + seq[i+1][-1])
+    return seq[0][-1]
 
-lines = aoc.input_as_lines("input/09a.txt")
+lines = aoc.input_as_lines("input/09.txt")
 lines = [list(map(lambda n: int(n), l.split(' '))) for l in lines]
-#part 1
-print('part 1:')
-part1(lines)
-
-# 1692591070
+res= [0,0]
+for l in lines:
+    res[0] += predictNext(l)
+    res[1] += predictPrev(l)
+print('part 1, part2:', res)
