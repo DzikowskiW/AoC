47a6b34648a3f0c9feeeb4a1a1eccfa676214272
Wojciech Dzikowski <dzikowskw@gmail.com>
2023-12-11 06:42:47 +0100

    AoC 2023 Day 11 both parts

--- aoc2023/11.py
+++ aoc2023/11.py
@@ -5,8 +5,11 @@ from collections import defaultdict
 import numpy as np
 from itertools import combinations
 
+PART1_MUL = 2
+PART2_MUL = 1000000
+
 def loop(lines):
-    print(lines)
+    # determine empty rows and cols
     double_x = set()
     double_y = set()
     galaxies = []
@@ -16,36 +19,33 @@ def loop(lines):
     for i, l in enumerate(lines.T):
         if np.all(l == '.'):
             double_x.add(i)
-    # print(double_y, double_x)
     
+    #find galaxies
     w = np.where(lines == '#')
     for i in range(len(w[0])):
-        galaxies.append((w[0][i], w[1][i]))
-    # print(galaxies)
-    print(len(list(combinations(galaxies, 2))))
+        galaxies.append((w[0][i], w[1][i])) 
     
+    #sum distances
     sum = 0
+    sum_empty = 0 
     for p1, p2 in combinations(galaxies,2):
         maxY = max(p1[0], p2[0])
         minY = min(p1[0], p2[0])
         maxX = max(p1[1], p2[1])
         minX = min(p1[1], p2[1])
-        countX = 0
-        countY = 0
         for y in range(minY,maxY):
-            countY += 1
-            if y in double_y: countY+= 1
+            sum += 1
+            if y in double_y: sum_empty += 1
         for x in range(minX,maxX):
-            countX += 1
-            if x in double_x: countX+= 1
-        count = countX + countY
-        sum+=count
-    print(sum)
+            sum += 1
+            if x in double_x: sum_empty += 1
+    part1 = sum+sum_empty*(PART1_MUL-1)
+    part2 = sum+sum_empty*(PART2_MUL-1)
+    print('part 1:', part1)
+    print('part 2:', part2)
         
     
 
 lines = aoc.input_as_lines("input/11.txt")
 lines = np.array(list(map(lambda l: [ *l], lines)))
-#part 1
-print('part 1:')
 loop(lines)
