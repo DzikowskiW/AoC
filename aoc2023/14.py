c00d9eb0b9603756b3ae91451f130e8aad21169b
Wojciech Dzikowski <dzikowskw@gmail.com>
2023-12-16 22:16:42 +0100

    AoC 2024 Day 14 part 2 brute force, but somehow correct

--- aoc2023/14.py
+++ aoc2023/14.py
@@ -1,26 +1,63 @@
 import numpy as np
+from collections import defaultdict
+import functools
+
+def score(lines):
+    score = 0
+    ylen = len(lines)
+    xlen = len(lines[0])
+    for i in range(ylen):
+        for j in range(xlen):
+            if lines[i][j] == 'O':
+                score += ylen - i
+    return score
+
+# @functools.cache
+def tilt(lines):
+    ylen = len(lines)
+    xlen = len(lines[0])
+    for _ in range(1, ylen):
+        for y in range(1, ylen):
+            for x in range(xlen):
+                if lines[y-1][x] == '.' and lines[y][x] == 'O':
+                    lines[y-1][x] = 'O'
+                    lines[y][x] = '.' 
+    return lines
+    
+def pretty_print(lines):
+    print('\n------------')
+    for l in lines:
+        print(''.join(list(map(str,l))))
+    # print(score(lines))
+
+def loop2(lines):
+    # pretty_print(lines)
+    last_scores = defaultdict(int)
+    scores_count = defaultdict(int)
+    for it in range(1,1000):   
+        for _ in range(4):
+            lines = tilt(lines)
+            lines = np.rot90(lines, 3)
+        scr = score(lines)
+        last_scores[scr] = it - last_scores[scr]
+        scores_count[scr] += 1
+        if it % 10 == 0:
+            print(str(it),': ', score(lines), 'last:', last_scores[scr])
+            print(sorted(scores_count.items(), key=lambda x:x[1], reverse=True))
+        # pretty_print(lines)
+        # print('ITER ', it+1)
+    print(sorted(scores_count.items(), key=lambda x:x[1], reverse=True))
 
 def loop(lines):
-    # print(lines)
-    llen=len(lines[0])
-    for i in range(1,len(lines)):
-        for j in range(1, len(lines)):
-            for k in range(llen):
-                if lines[j-1][k] == '.' and lines[j][k] == 'O':
-                    lines[j-1][k] = 'O'
-                    lines[j][k] = '.'
-    # print('-----')                
-    # print(lines)
-    w = 0
-    for i in range(len(lines)):
-        for j in range(llen):
-            if lines[i][j] == 'O':
-                w += len(lines) - i
-    print(w)
+    lines = tilt(lines)
+    pretty_print(lines)
+    
+# print(rotate([[1,2,3], [4,5,6], [7,8,9]]))
 
 with open("input/14.txt") as f:
     input = f.read().rstrip("\n").split("\n\n")
     input = [[[*l] for l in ll.split("\n")] for ll in input]
     input = [np.array(s) for s in input]
-    loop(input[0])
+    loop2(input[0])
     
+ 
