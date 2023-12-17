bb2552adfc4d612379fa62ea0bf1d5fc5316ca1b
Wojciech Dzikowski <dzikowskw@gmail.com>
2023-12-18 00:14:43 +0100

    AoC 2023 Day 17 Part 2

--- aoc2023/17.py
+++ aoc2023/17.py
@@ -4,32 +4,13 @@ import sys
 from queue import PriorityQueue
 
 EDGES = ((-1,0), (0,1),(1,0),(0,-1))
-MAX_STRAIGHT = 3
-
-def is_maxed_out(y,x,dy,dx, prev_nodes):
-    if y == x == 0:
-        return False
-    node = (y,x)
-    
-    straights = 0
-    i = 1
-    while True:
-        node = prev_nodes[node[0]][node[1]]
-        if node == 0 or node[0] != y - dy*i or node[1] != x - dx*i:
-            break
-        # print(node[0], dy*i, '|', node[1], x + dx*i)
-        straights +=1
-        i+=1
-    # when someone enters block, they turn next turn, i.e. ^>>>, so it adds up to 4
-    return straights > MAX_STRAIGHT
    
-   
-def kind_of_dijsktra(graph):
+def kind_of_dijsktra(graph, part = 1):
     ylen = len(graph)
     xlen = len(graph[0])
 
     pq = PriorityQueue()
-    # dist, coords, , last edge, edge count
+    # dist, coords, last edge, edge count
     pq.put((0, (0,0), -1, -1))    
     shortest = defaultdict(int)
     while not pq.empty():
@@ -41,13 +22,20 @@ def kind_of_dijsktra(graph):
         for dy,dx in EDGES:
             yy = y + dy
             xx = x + dx
-            ddirlen = 0
+            ddirlen = 1
             # don't go back
             if last_dir == (-dy, -dx):
                 continue
             if last_dir == (dy,dx):
                 ddirlen = dirlen + 1
-            if ddirlen >= 3:
+            if part == 1 and ddirlen > 3:
+                continue
+            if part == 2 and ddirlen > 10:
+                continue
+            # same path OK
+            # above 4 OK
+            #from start OK
+            if part == 2 and not (last_dir == (dy,dx) or dirlen >= 4 or coords == (0,0)) :
                 continue
             if 0 <= yy < ylen and 0 <= xx < xlen:
                 pq.put((dist + graph[yy][xx], (yy,xx), (dy, dx), ddirlen))
@@ -58,12 +46,9 @@ def kind_of_dijsktra(graph):
             res = min(res, dist)
     return res
 
-def loop(city):
-    return kind_of_dijsktra(city) 
-    
-
 with open("input/17.txt") as f:
     lines = f.read().rstrip().split('\n')
     lines = [[int(c) for c in l] for l in lines]
-    lines = np.array(lines, dtype=int)
-    print('Part 1:', loop(lines))
+    city = np.array(lines, dtype=int)
+    print('Part 1:', kind_of_dijsktra(city, part=1))    
+    print('Part 2:', kind_of_dijsktra(city, part=2))
