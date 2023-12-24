6b0214190b374474facb61b3fbedd124cc210981
Wojciech Dzikowski <dzikowskw@gmail.com>
2023-12-24 10:10:55 +0100

    AoC Day 24 both parts

--- aoc2023/24.py
+++ aoc2023/24.py
@@ -4,6 +4,7 @@ from heapq import heappop, heappush
 import functools
 from copy import deepcopy
 from itertools import combinations
+import z3
 
 
 # LIMITS = (7,27) 
@@ -32,9 +33,29 @@ def part1(paths, limits = LIMITS):
                                 # print(x1,y1, dx1, dy1,'|', x3,y3, 'res:', px, py)
                                 count += 1
     print('Part1:', count)
-            
+
+def part2(paths):
+    s = z3.Solver()
+    
+    x, y, z = z3.Real('x'), z3.Real('y'), z3.Real('z')
+    dx, dy, dz = z3.Real('dx'), z3.Real('dy'), z3.Real('dz')
+
+    for i, p in enumerate(paths):
+        (xx, yy, zz), (dxx, dyy, dzz) = p
+        t = z3.Real('t'+str(i))
+        s.add(x + dx * t == xx + dxx * t)
+        s.add(y + dy * t == yy + dyy * t)
+        s.add(z + dz * t == zz + dzz * t)
+        s.add(t >= 0)
+    
+    s.check()
+    x = s.model().eval(x).as_long()
+    y = s.model().eval(y).as_long()
+    z = s.model().eval(z).as_long()
+    print('part 2:', x + y + z)
 
 with open("input/24.txt") as f:
     lines = f.read().rstrip().split('\n')
     paths = [(tuple(map(int,ll[0].split(', '))), tuple(map(int,ll[1].split(', ')))) for ll in (l.split(' @ ') for l in lines)]
-    part1(paths)
+    part1(paths)   
+    part2(paths)
