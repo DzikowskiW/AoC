2b5025cf42c5ee175ca1c89a564972f418f1c05f
Wojciech Dzikowski <dzikowskw@gmail.com>
2023-12-29 00:04:59 +0100

    AoC 2022 Day 22 Part 2

--- aoc2022/22p2.py
+++ aoc2022/22p2.py
@@ -1,5 +1,3 @@
-import re
-
 N = -1
 S = 1
 W = complex(0, -1)
@@ -9,57 +7,66 @@ R = pow(complex(0,1), 3)
 L = pow(complex(0,1), 1)
 
 EDGE = 50
-START = complex(5, 60)
+START = complex(0, 50)
 
-HISTORY = set([])
+# HISTORY = set([])
 
 def wrap(y,x, dir):
     global N, S, W, E
-    print(dir, y//EDGE, x//EDGE)
-    match (dir, y//EDGE, x//EDGE):
-        case (N, _, 0): print('N0');return complex(50 + x, 50), E
-        case (N, _, 1): print('N1');return complex(100 + x, 0), E
-        case (N, _, 2): print('N2');return complex(149, x - 100), N
-        case (S, _, 0): print('S0'); return complex(0, x + 100), S
-        case (S, _, 1): print('S1'); return complex(150 + x, 49), W
-        case (S, _, 2): print('S2'); return complex(x - 50, 99), W
-        case (E, 0, _): return complex(149 - y, 99), W 
-        case (E, 1, _): return complex(y + 50, 49), N
-        case (E, 2, _): return complex(149 - y, 149), W
-        case (E, 3, _): return complex(149, y - 100), N
-        case (W, 0, _): return complex(149 - y, 0), E
-        case (W, 1, _): return complex(100, y-50), S
-        case (W, 2, _): return complex(149 - y, 50), E
-        case (W, 3, _): return complex(0, y- 50), S
-
-    assert(False)
+    ey = y // EDGE
+    ex = x // EDGE
+    if dir == N:
+        if ex == 0: return complex(50 + x, 50), E
+        elif ex == 1: return complex(100 + x, 0), E
+        elif ex == 2: return complex(199, x - 100), N
+    elif dir == S:
+        if ex == 0: return complex(0, x + 100), S
+        if ex == 1: return complex(100 + x, 49), W
+        if ex == 2: return complex(x - 50, 99), W
+    elif dir == E:
+        if ey == 0: return complex(149 - y, 99), W 
+        if ey == 1: return complex(49, 50 + y), N
+        if ey == 2: return complex(149 - y, 149), W
+        if ey == 3: return complex(149, y - 100), N
+    elif dir == W:
+        if ey == 0: return complex(149 - y, 0), E
+        if ey == 1: return complex(100, y-50), S
+        if ey == 2: return complex(149 - y, 50), E
+        if ey == 3: return complex(0, y - 100), S
     
-def test():
-    assert(wrap())
 def walk(cube, head, val, turn):
     pos, dir = head
     # walk
+    turn_val = 0
+    log = False
+    ndir = dir
+    if dir == N: turn_val = 3
+    if dir == W: turn_val = 2
+    if dir == S: turn_val = 1
+    print('step', int(head[0].real)+1, int(head[0].imag)+1, turn_val, val)
     for _ in range(val):
         nxt:complex = pos + dir
         ny, nx = int(nxt.real), int(nxt.imag)
         # check next coords
         if not (0 <= ny < 4*EDGE and 0 <= nx < len(cube[ny])) or (cube[ny][nx] not in ['.','#']):
-            print('PPPPPP', ny, nx, dir)
-            npos, dir = wrap(ny, nx, dir)
+            npos, ndir = wrap(ny, nx, dir)
             ny, nx = int(npos.real), int(npos.imag)
-            print('U', ny, nx, dir)
-
         # check next symbol on cube
         if cube[ny][nx] == '#':
-            pos = complex(ny,nx)
-            # break
+            break
         elif cube[ny][nx] == '.':
             pos = complex(ny,nx)
+            dir = ndir
+            # HISTORY.add((int(pos.real),int(pos.imag),'X'))
         else:
             assert(False)
-        HISTORY.add((int(pos.real),int(pos.imag),'X'))
 
     #turn
+    turn_val = 0
+    if dir == N: turn_val = 3
+    if dir == W: turn_val = 2
+    if dir == S: turn_val = 1
+    print('step', int(pos.real)+1, int(pos.imag)+1, turn_val, turn)
     if turn == 'L':
         dir *= L
     elif turn == 'R':
@@ -83,14 +90,20 @@ def process(cube, path):
         ipath += 1
     if len(tmp) > 0:
         head = walk(cube, head, int(tmp), None)
-    to_file(cube, HISTORY)
+    fy, fx = int(head[0].real), int(head[0].imag)
+    turn_val = 0
+    if head[1] == N: turn_val = 3
+    if head[1] == W: turn_val = 2
+    if head[1] == S: turn_val = 1
+    print(fy+1,fx+1,turn_val)
+    print(1000*(fy +1) + 4* (fx + 1) + turn_val)
+    # to_file(cube, HISTORY)
     return
             
 def to_file(cube,history):
     f = open("output/22o.txt","w")
     out = [[*l] for l in cube]
     for y,x,c in history:
-        # print(y,x,c)
         out[y][x] = c
     for l in out:
         f.write(''.join(l))
@@ -100,6 +113,5 @@ def to_file(cube,history):
 lines = open("22.txt").read().rstrip().split('\n')
 path = lines.pop()
 lines.pop()
-path = '198'
 path = path
 process(lines, path)
